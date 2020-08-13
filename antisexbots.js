2c4e646974bf30dcfd94d503a23ec37c239387fe
zenwaichi <z3nw41ch1@gmail.com>
2020-08-13 02:47:26 +0200

    Update antisexbots.js

--- antisexbots.js
+++ antisexbots.js
@@ -14,7 +14,11 @@ var func = function(){
 var target = document.querySelectorAll('.chat_message_window')[1],
 conf = { childList: true, subtree: true },
 func = (ml,ob)=>{for(var m of ml){
-if(m.addedNodes&&m.addedNodes[0].innerText.includes('freegirls.today')){m.addedNodes[0].style.display='none'}}};
+if(m.addedNodes&&
+   (m.addedNodes[0].innerText.includes('freegirls.today')||
+    m.addedNodes[0].innerText.includes('My nickname J')))
+{m.addedNodes[0].style.display='none'}}};
+if(!target)throw('---UNABLE TO LOCATE CHAT WINDOW---')
 self.obs = new MutationObserver(func)
 self.obs.observe(target,conf)
 console.log('anti sex bot')
