5923f78f7af00809d940f7a6bd399ea9d9196a5a
zenwaichi <z3nw41ch1@gmail.com>
2020-09-09 02:29:39 +0200

    Update antisexbots.user.js

--- antisexbots.user.js
+++ antisexbots.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         anti sex bots
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  try to take over the world!
 // @author       zenwaichi
 // @match        https://www.kongregate.com/games/*
+// @match        http://www.kongregate.com/games/*
 // @grant        none
 // @updateURL
 // ==/UserScript==
@@ -18,16 +19,17 @@
                 var _
                 if(m.addedNodes&&
                    (_=m.addedNodes[0],
-                    _.innerText.includes('freegirls.today')||
+                    _.innerText.includes('dateforsex.net')||
                     _.innerText.includes('My nickname J')||
                     _.innerText.includes('𝕗𝕣𝕖𝕖𝕘𝕚𝕣𝕝𝕤.𝕥𝕠𝕕𝕒𝕪')
                    )
                 )
                 {m.addedNodes[0].style.display='none'}}};
-        if(!target)throw('NO CHAT WINDOW')
+        console.log('target = ',target)
+        if(!target){console.error('NO CHAT WINDOW');window.setTimeout(func,5000);return}
         self.obs = new MutationObserver(func)
         self.obs.observe(target,conf)
-        console.log('anti sex bot')
+        console.log('anti sex bots ACTIVE')
     }
     window.setTimeout(func,10000)
 })();
