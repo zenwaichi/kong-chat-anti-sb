c4cf5d4ca72077dea193053b8196bdcfacc427ef
zenwaichi <z3nw41ch1@gmail.com>
2020-09-11 08:58:49 +0200

    Update antisexbots.user.js

--- antisexbots.user.js
+++ antisexbots.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         anti sex bots
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  try to take over the world!
 // @author       zenwaichi
 // @match        https://www.kongregate.com/games/*
@@ -12,24 +12,40 @@
 
 (function() {
     'use strict';
-    var func = function(){
-        var target = document.querySelectorAll('.chat_message_window')[1],
-            conf = { childList: true, subtree: true },
-            func = (ml,ob)=>{for(var m of ml){
-                var _
-                if(m.addedNodes&&
-                   (_=m.addedNodes[0],
-                    _.innerText.includes('dateforsex.net')||
-                    _.innerText.includes('My nickname J')||
-                    _.innerText.includes('𝕗𝕣𝕖𝕖𝕘𝕚𝕣𝕝𝕤.𝕥𝕠𝕕𝕒𝕪')
-                   )
-                )
-                {m.addedNodes[0].style.display='none'}}};
+    var target;
+    var wrap = function(){
+        target = document.querySelectorAll('.chat_message_window')[1]
+        var conf = { childList: true, subtree: true },
+            func = (ml,ob)=>{
+                for(var m of ml){
+                    var _
+                    if(m.addedNodes&&
+                      (_=m.addedNodes[0].innerText,
+                       _.includes('dateforsex.net')||
+                       _.includes('My nickname J')||
+                       _.includes('𝕗𝕣𝕖𝕖𝕘𝕚𝕣𝕝𝕤.𝕥𝕠𝕕𝕒𝕪')
+                      )
+                    )
+                    {
+                        m.addedNodes[0].style.display='none'
+                    }
+                }
+            };
         console.log('target = ',target)
-        if(!target){console.error('NO CHAT WINDOW');window.setTimeout(func,5000);return}
+        if(!target){
+            console.error('NO CHAT WINDOW')
+            window.setTimeout(wrap,5000)
+            return
+        }
         self.obs = new MutationObserver(func)
         self.obs.observe(target,conf)
         console.log('anti sex bots ACTIVE')
     }
-    window.setTimeout(func,10000)
+    window.setTimeout(wrap,10000)
+    window.setTimeout(()=>{
+        window.setInterval(()=>{
+            if(document.querySelectorAll('.chat_message_window')[1]
+               !== target) wrap()
+        },5000)
+    },20000)
 })();
